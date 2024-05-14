cbc28abc528796ece13c98c89e91fddb05317d76
Dainerdev <florianarroyo15@gmail.com>
2024-05-13 19:19:56 -0500

    Validaciones e inicio en uso de planttillas

--- src/main/java/co/edu/unicartagena/webspring/controllers/ControladorInicio.java
+++ src/main/java/co/edu/unicartagena/webspring/controllers/ControladorInicio.java
@@ -3,10 +3,12 @@ package co.edu.unicartagena.webspring.controllers;
 import co.edu.unicartagena.webspring.models.Emisora;
 import co.edu.unicartagena.webspring.services.IEmisoraService;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +36,10 @@ public class ControladorInicio {
     }
 
     @PostMapping("/guardar")
-    public String guardar(@ModelAttribute("emisora") Emisora emi) {
+    public String guardar(@ModelAttribute("emisora") @Valid Emisora emi, Errors errors) {
+        if (errors.hasErrors()) {
+            return "modify";
+        }
         emisoraService.guardar(emi);
         return "redirect:/";
     }
--- src/main/java/co/edu/unicartagena/webspring/models/Emisora.java
+++ src/main/java/co/edu/unicartagena/webspring/models/Emisora.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Entity
@@ -15,8 +16,11 @@ public class Emisora implements Serializable{
     private static final long serialVersionUID = 1L;
     @Id
     private int id;
+    @NotEmpty
     private String nombre;
+    @NotEmpty
     private String frecuencia;
+    @NotEmpty
     private String transmision;
     
 }
