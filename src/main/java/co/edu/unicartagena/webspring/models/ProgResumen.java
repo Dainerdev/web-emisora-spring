76d5314beab5fe6b6c053e9aec29476ed6de621a
Dainerdev <florianarroyo15@gmail.com>
2024-05-15 16:50:17 -0500

    Creacion de la clase Programa Resumen (ProgResumen)

---
+++ src/main/java/co/edu/unicartagena/webspring/models/ProgResumen.java
@@ -0,0 +1,26 @@
+
+package co.edu.unicartagena.webspring.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Entity
+@Table(name = "progresumen")
+@Data
+public class ProgResumen implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    private int id;
+    @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String programa;
+    @NotEmpty
+    private int consorcio;
+}
